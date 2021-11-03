7dab29fd8308e99cfaa899f45dabe1aed2d30422
Gislaine Amancio <gimcjf@gmail.com>
2021-11-03 10:03:28 -0300

    Criação da classe Prato

---
+++ model/Prato.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Prato extends Base {
+
+    public String formato;
+    public String cor;
+    public int ano;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Prato) {
+            Prato novoPrato = (Prato) obj;
+            if (this.id == novoPrato.id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String msg = "Id prato: " + super.id + ", Formato: " + this.formato + ", Cor: " + this.cor + ", Ano: "
+                + this.ano;
+        return msg;
+    }
+
+}
